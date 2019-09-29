d5f75c83d5a56b26ad5c365e43600454dab7c840
Paul M <paulmcadory@gmail.com>
2019-09-29 19:22:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ function updateObjectWithKeyAndValue(recipes, key, value) {
   return Object.assign({}, recipes, { [key]: value});
 }
 function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
-  recipes[key] = value
+  recipes[key] = value;
+  
 }
